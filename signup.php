85987d032c380778c29bff4d4da51d60050be216
Flying930 <74603417+Flying930@users.noreply.github.com>
2021-12-15 22:13:20 +0800

    update

--- signup.php
+++ signup.php
@@ -6,13 +6,13 @@
     if(!isset($_POST['submit'])){
         exit("錯誤執行");
     }//判斷是否有submit操作
-    $name=$_POST['user_id'];//post獲取表單裡的name
-    $account=$_POST['user_account'];//post獲取表單裡的accountnumber
-    $password=$_POST['user_password'];//post獲取表單裡的password
+    $name=$_POST['name'];//post獲取表單裡的name
+    $account=$_POST['account'];//post獲取表單裡的accountnumber
+    $password=$_POST['password'];//post獲取表單裡的password
     $q="insert into `user`(`id`,`user_id`,`user_account`,`user_password`) values (null,'$name','$account','$password')";//向資料庫插入表單傳來的值的sql
     $reslut=mysql_query($q,$con);//執行sql
-    echo $q;
-    console.log($q);
+    echo $account;
+    console.log($name);
     
 ?> 
 </body>
