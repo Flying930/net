6902e2b44fa3ce2d2b4b405dbed2bc9f4a7703eb
Flying930 <74603417+Flying930@users.noreply.github.com>
2021-12-15 22:50:53 +0800

    update

--- signup.php
+++ signup.php
@@ -1,7 +1,12 @@
 <html>
 <body>
 <?php
-    include('connect.php');//連結資料庫 
+    include('connect.php');//連結資料庫
+    $link = @mysqli_connect( 
+        'us-cdbr-east-05.cleardb.net',  // MySQL主機名稱 
+        'b184b9cfe884e3',       // 使用者名稱 
+        '53020e02',  // 密碼 
+        'heroku_d320a3179e97ee9');   
     header("Content-Type: text/html; charset=utf8");
     if(!isset($_POST['submit'])){
         exit("錯誤執行");
@@ -13,7 +18,6 @@
     $q="insert into `user`(`user_id`,`user_account`,`user_password`) values ('$name','$account','$password')";//向資料庫插入表單傳來的值的sql
     echo $q;
     echo $link;
-    return ;
 
     if ($link->query($q) === TRUE) {
         echo "新记录插入成功";
