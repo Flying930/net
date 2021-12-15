34c762c5a6784caea39bb4bb4e077694683f4876
Flying930 <74603417+Flying930@users.noreply.github.com>
2021-12-15 21:54:38 +0800

    update

--- signup.php
+++ signup.php
@@ -18,7 +18,9 @@ mysqli_close($link);  // 關閉資料庫連接
 ?>
 <?php 
     header("Content-Type: text/html; charset=utf8");
-    
+    if(!isset($_POST['submit'])){
+        exit("錯誤執行");
+    }//判斷是否有submit操作
 ?> 
 </body>
 </html>
