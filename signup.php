999a8d073f794535fb388459fc75d20863f4f5dd
Flying930 <74603417+Flying930@users.noreply.github.com>
2021-12-15 22:06:14 +0800

    update

--- signup.php
+++ signup.php
@@ -10,6 +10,7 @@
     $account=$_POST['user_account'];//post獲取表單裡的accountnumber
     $password=$_POST['user_password'];//post獲取表單裡的password
     $q="insert into `user`(`id`,`user_id`,`user_account`,`user_password`) values (null,'$name','$account','$password')";//向資料庫插入表單傳來的值的sql
+    $reslut=mysql_query($q,$con);//執行sql
     
 ?> 
 </body>
