e2d20b226148ceae6e84faf1d86581e17552b82c
Flying930 <74603417+Flying930@users.noreply.github.com>
2021-12-15 21:48:42 +0800

    update

--- connect.php
+++ connect.php
@@ -1,11 +1,16 @@
 <?php
-    $server="us-cdbr-east-05.cleardb.net";//主機
-    $db_username="b184b9cfe884e3";//資料庫使用者名稱
-    $db_password="53020e02";//資料庫使用者密碼
-    $db_database="heroku_d320a3179e97ee9";
-    $con = mysql_connect($server,$db_username,$db_password,$db_database);//連結資料庫
-    if(!$con){
-        die("can't connect".mysql_error());//如果連結失敗輸出錯誤
-    }
-    mysql_select_db('test',$con);//選擇資料庫（我的是test）
+// 建立MySQL的資料庫連接 
+$link = @mysqli_connect( 
+            'us-cdbr-east-05.cleardb.net',  // MySQL主機名稱 
+            'b184b9cfe884e3',       // 使用者名稱 
+            '53020e02',  // 密碼 
+            'heroku_d320a3179e97ee9');  
+if ( !$link ) {
+   echo "MySQL資料庫連接錯誤!<br/>";
+   exit();
+}
+else {
+   echo "MySQL資料庫test連接成功!<br/>";
+}
+mysqli_close($link);  // 關閉資料庫連接
 ?>
