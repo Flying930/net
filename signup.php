fc6165d3f8edbf1d19bd1b79ee57bcfac7b255f7
Flying930 <74603417+Flying930@users.noreply.github.com>
2021-12-15 22:00:25 +0800

    update

--- signup.php
+++ signup.php
@@ -1,22 +1,7 @@
 <html>
 <body>
 <?php
-// 建立MySQL的資料庫連接 
-$link = @mysqli_connect( 
-            'us-cdbr-east-05.cleardb.net',  // MySQL主機名稱 
-            'b184b9cfe884e3',       // 使用者名稱 
-            '53020e02',  // 密碼 
-            'heroku_d320a3179e97ee9');  
-if ( !$link ) {
-   echo "MySQL資料庫連接錯誤!<br/>";
-   exit();
-}
-else {
-   echo "MySQL資料庫test連接成功!<br/>";
-}
-mysqli_close($link);  // 關閉資料庫連接
-?>
-<?php 
+    include('connect.php');//連結資料庫 
     header("Content-Type: text/html; charset=utf8");
     if(!isset($_POST['submit'])){
         exit("錯誤執行");
@@ -24,7 +9,6 @@ mysqli_close($link);  // 關閉資料庫連接
     $name=$_POST['user_id'];//post獲取表單裡的name
     $account=$_POST['user_account'];//post獲取表單裡的accountnumber
     $password=$_POST['user_password'];//post獲取表單裡的password
-    include('connect.php');//連結資料庫
     
 ?> 
 </body>
