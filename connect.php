ac2504b85b2df1a9baf036310a04af0d5f098e16
Flying930 <74603417+Flying930@users.noreply.github.com>
2021-12-16 19:38:20 +0800

    update

--- connect.php
+++ connect.php
@@ -4,7 +4,8 @@ $link = @mysqli_connect(
             'us-cdbr-east-05.cleardb.net',  // MySQL主機名稱 
             'b184b9cfe884e3',       // 使用者名稱 
             '53020e02',  // 密碼 
-            'heroku_d320a3179e97ee9');  
+            'heroku_d320a3179e97ee9');
+echo $link;  
 if ( !$link ) {
    echo "MySQL資料庫連接錯誤!<br/>";
    exit();
