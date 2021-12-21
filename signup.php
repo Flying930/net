47aaa52dfea23c5a3830d954c3939f70de0da824
Flying930 <74603417+Flying930@users.noreply.github.com>
2021-12-21 23:23:05 +0800

    update

--- signup.php
+++ signup.php
@@ -3,14 +3,15 @@
 <?php
     include('connect.php');//連結資料庫
     //header("Content-Type: text/html; charset=utf8");
-    if(!isset($_POST['submit'])){
+    /*if(!isset($_POST['submit'])){
         exit("錯誤執行");
-    }//判斷是否有submit操作
-    echo "AAA";
-    /*$name=$_POST['name'];//post獲取表單裡的name
+    }//判斷是否有submit操作*/
+    echo "AAAA";
+    if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $name=$_POST['name'];//post獲取表單裡的name
     $password=$_POST['password'];//post獲取表單裡的password
-    echo "AAA=$name,$password";return;
-    $q="use `heroku_d320a3179e97ee9`; insert into `user`(`user_id`,`user_password`) values ('$name','$password')";//向資料庫插入表單傳來的值的sql
+    echo "AAA=$name,$password";
+    /*$q="use `heroku_d320a3179e97ee9`; insert into `user`(`user_id`,`user_password`) values ('$name','$password')";//向資料庫插入表單傳來的值的sql
     echo "$q";return;
     echo "$link";
     //return;
@@ -30,7 +31,7 @@
         echo "新记录插入成功";
     } else {
         echo "Error: " . $q . "<br>" . $link->error;
-    }*/
+    }*/}
 ?> 
 </body>
 </html>
