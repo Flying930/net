e36de12a35a601b7cc6012ccb649856cf8dd3bf8
Flying930 <74603417+Flying930@users.noreply.github.com>
2021-12-21 22:59:50 +0800

    update

--- signup.php
+++ signup.php
@@ -1,11 +1,12 @@
 <html>
 <body>
 <?php
+    echo "AAA";
     include('connect.php');//連結資料庫
     //header("Content-Type: text/html; charset=utf8");
     /*if(!isset($_POST['submit'])){
         exit("錯誤執行");
-    }//判斷是否有submit操作*/
+    }//判斷是否有submit操作
     $name=$_POST['name'];//post獲取表單裡的name
     $password=$_POST['password'];//post獲取表單裡的password
     echo "AAA=$name,$password";return;
