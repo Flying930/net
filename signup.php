bceea959ce6ef2d386b147803adbfc7e7fcf39fc
Flying930 <74603417+Flying930@users.noreply.github.com>
2021-12-21 23:07:25 +0800

    update

--- signup.php
+++ signup.php
@@ -1,8 +1,8 @@
 <html>
 <body>
 <?php
-    echo "AAA";
     include('connect.php');//連結資料庫
+    echo "AAA";
     //header("Content-Type: text/html; charset=utf8");
     /*if(!isset($_POST['submit'])){
         exit("錯誤執行");
