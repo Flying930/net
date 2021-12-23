3681811179b68d590a9f264df673d30c52ef02cb
Flying930 <74603417+Flying930@users.noreply.github.com>
2021-12-23 19:48:17 +0800

    update

--- login.php
+++ login.php
@@ -5,16 +5,16 @@
     $password = $_POST['password'];//post獲得使用者密碼單值
     $password_hash=password_hash($password,PASSWORD_DEFAULT);//hash加密
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-        $q = "select * from `user` where user_name = '$name'";//檢測資料庫是否有對應的username的sql
-        $result = mysql_query($link,$q);//執行sql
-        echo $result;
-        /*if(mysqli_num_rows($result)==1 && $password==mysqli_fetch_assoc($result)["password"]){
-            session_start();
-            $_SESSION["loggedin"] = true;
-            $_SESSION["name"] = mysqli_fetch_assoc($result)["name"];
-            header("location:首頁.html");
-        }*/
-        //else{function_alert("帳號或密碼錯誤"); }
+        $q = "select * from `user` where user_name = '$name' and user_password = '$password'";//檢測資料庫是否有對應的username的sql
+        if($link->query($q) === TRUE){
+            if(mysqli_num_rows($result)==1 && $password==mysqli_fetch_assoc($result)["password"]){
+                session_start();
+                $_SESSION["loggedin"] = true;
+                $_SESSION["name"] = mysqli_fetch_assoc($result)["name"];
+                header("location:首頁.html");
+            }
+            //else{function_alert("帳號或密碼錯誤"); }
+        }
     }
     //else{function_alert("Something wrong");}
     /*else{//如果使用者名稱或密碼有空
