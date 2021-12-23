1cf88adbb5b4e115f9365cab96b97ff689bf4279
Flying930 <74603417+Flying930@users.noreply.github.com>
2021-12-23 18:19:47 +0800

    update

--- signup.php
+++ signup.php
@@ -7,37 +7,18 @@
         exit("錯誤執行");
     }//判斷是否有submit操作*/
     if($_SERVER["REQUEST_METHOD"]=="POST"){
-    $name=$_POST['name'];//post獲取表單裡的name
-    $password=$_POST['password'];//post獲取表單裡的password
-    echo "aa   ";
-    $q="INSERT INTO user (user_name,user_password) VALUES ('$name','$password')";//向資料庫插入表單傳來的值的sql
-    echo $q;
-    if ($link->query($q) === TRUE) {
-        echo "註冊成功";//成功輸出註冊成功
-        echo "<a href='index.html'>未成功跳轉頁面請點擊此</a>";
-            header("refresh:32;url=index.html");
-            exit;
-    } else {
-        echo "Error ";
+        $name=$_POST['name'];//post獲取表單裡的name
+        $password=$_POST['password'];//post獲取表單裡的password
+        $q="INSERT INTO user (user_name,user_password) VALUES ('$name','$password')";//向資料庫插入表單傳來的值的sql
+        if ($link->query($q) === TRUE) {
+            echo "註冊成功";//成功輸出註冊成功
+            echo "<a href='index.html'>未成功跳轉頁面請點擊此</a>";
+                header("refresh:3;url=index.html");
+                exit;
+        } else {
+            echo "Error ";
+        }
     }
-    /*
-    $reslut=mysql_query($link,$q);//執行sql
-    if (!$reslut){
-        die('Error: ' . mysql_error());//如果sql執行失敗輸出錯誤
-    }
-    else{
-        echo "註冊成功";//成功輸出註冊成功
-        echo ?>"
-        <script>
-        setTimeout(function(){window.location.href='index.html';},1000);
-        </script>";<?php
-    }
-    //try{
-    /*if ($link->query($q) === TRUE) {
-        echo "新记录插入成功";
-    } else {
-        echo "Error: " . $q . "<br>" . $link->error;
-    }*/}
 ?> 
 </body>
 </html>
