87d474892896753e457331dd68818d6d5b8dcc54
Flying930 <74603417+Flying930@users.noreply.github.com>
2021-12-30 12:31:13 +0800

    update

--- login.php
+++ login.php
@@ -1,13 +1,13 @@
 <?PHP
     //header("Content-Type: text/html; charset=utf8");
     include('connect.php');//連結資料庫
-    $name=$_POST['name'];//post獲取表單裡的name
-    $password = $_POST['password'];//post獲得使用者密碼單值
-    $password_hash=password_hash($password,PASSWORD_DEFAULT);//hash加密
     if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $name=$_POST['name'];//post獲取表單裡的name
+        $password = $_POST['password'];//post獲得使用者密碼單值
+        $password_hash=password_hash($password,PASSWORD_DEFAULT);//hash加密
         $q = "SELECT * FROM user WHERE user_name = '$name' AND user_password = '$password'";//檢測資料庫是否有對應的username的sql
         echo $q;
-        if(mysqli_query($q) === TRUE){
+        if($link->query($q) === TRUE){
             echo "  bb";
             /*if(mysqli_num_rows($result)==1 && $password==mysqli_fetch_assoc($result)["password"]){
                 session_start();
