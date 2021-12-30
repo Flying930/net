99da765e06e4553d9eefb6a3462f36bb0582ff50
Flying930 <74603417+Flying930@users.noreply.github.com>
2021-12-30 20:53:03 +0800

    update

--- login.php
+++ login.php
@@ -6,7 +6,7 @@
     $password_hash=password_hash($password,PASSWORD_DEFAULT);//hash加密
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         $q = "SELECT * FROM user WHERE user_name = '".$name."' AND user_password = '".$password."'";//檢測資料庫是否有對應的username的sql
-        if($link->query($q) === TRUE){
+        /*if($link->query($q) === TRUE){
             if(mysqli_num_rows($result)==1 && $password==mysqli_fetch_assoc($result)["password"]){
                 session_start();
                 $_SESSION["loggedin"] = true;
@@ -15,7 +15,8 @@
             }
             else{function_alert("帳號或密碼錯誤"); }
         }
-        else{function_alert("Something wrong");}
+        else{function_alert("Something wrong");}*/
+        echo $q;
     }
     else{//如果使用者名稱或密碼有空
         echo "表單填寫不完整! 2秒後將跳至登入頁面~";
