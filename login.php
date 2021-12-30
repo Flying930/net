c2440374146c277d962cc78bb116d6811b9c5944
Flying930 <74603417+Flying930@users.noreply.github.com>
2021-12-30 13:11:56 +0800

    update

--- login.php
+++ login.php
@@ -4,27 +4,27 @@
     $name=$_POST['name'];//post獲取表單裡的name
     $password = $_POST['password'];//post獲得使用者密碼單值
     $password_hash=password_hash($password,PASSWORD_DEFAULT);//hash加密
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
+    /*if($_SERVER["REQUEST_METHOD"] == "POST"){
         $q = "SELECT * FROM user WHERE user_name = '$name' AND user_password = '$password'";//檢測資料庫是否有對應的username的sql
-        if(mysqli_query($link,$q) === TRUE){
+        if($link->query($q) === TRUE){
             echo "  bb";
-            /*if(mysqli_num_rows($result)==1 && $password==mysqli_fetch_assoc($result)["password"]){
+            if(mysqli_num_rows($result)==1 && $password==mysqli_fetch_assoc($result)["password"]){
                 session_start();
                 $_SESSION["loggedin"] = true;
                 $_SESSION["name"] = mysqli_fetch_assoc($result)["name"];
                 header("location:首頁.html");
             }
-            //else{function_alert("帳號或密碼錯誤"); }*/
+            //else{function_alert("帳號或密碼錯誤"); }
         }
     }
     else{function_alert("Something wrong");}
-    /*else{//如果使用者名稱或密碼有空
+    else{//如果使用者名稱或密碼有空
         echo "表單填寫不完整! 2秒後將跳至登入頁面~";
         echo "<a href='index.html'>未成功跳轉頁面請點擊此</a>";
                 header("refresh:2;url=index.html");
                 exit;
         //如果錯誤 2秒後跳轉到登入頁面重試;
-    }*/
+    }
     function function_alert($message) { 
       
         // Display the alert box  
@@ -32,6 +32,6 @@
          window.location.href='index.html';
         </script>"; 
         return false;
-    } 
+    } */
     mysql_close();//關閉資料庫
 ?>
