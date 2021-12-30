a269e3d70a50078360b4e37243704acf83524b4b
Flying930 <74603417+Flying930@users.noreply.github.com>
2021-12-30 20:37:13 +0800

    update

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?PHP
     //header("Content-Type: text/html; charset=utf8");
-    /*include('connect.php');//連結資料庫
+    include('connect.php');//連結資料庫
     $name=$_POST['name'];//post獲取表單裡的name
     $password = $_POST['password'];//post獲得使用者密碼單值
     $password_hash=password_hash($password,PASSWORD_DEFAULT);//hash加密
@@ -8,13 +8,13 @@
         $q = "SELECT * FROM user WHERE user_name = '$name' AND user_password = '$password'";//檢測資料庫是否有對應的username的sql
         if($link->query($q) === TRUE){
             echo "  bb";
-            if(mysqli_num_rows($result)==1 && $password==mysqli_fetch_assoc($result)["password"]){
+            /*if(mysqli_num_rows($result)==1 && $password==mysqli_fetch_assoc($result)["password"]){
                 session_start();
                 $_SESSION["loggedin"] = true;
                 $_SESSION["name"] = mysqli_fetch_assoc($result)["name"];
                 header("url=首頁.html");
             }
-            else{function_alert("帳號或密碼錯誤"); }
+            else{function_alert("帳號或密碼錯誤"); }*/
         }
     }
     else{function_alert("Something wrong");}
@@ -33,41 +33,5 @@
         </script>"; 
         return false;
     } 
-    mysql_close();//關閉資料庫*/
-$conn=require_once "connect.php";
-
-$name=$_POST["name"];
-$password=$_POST["password"];
-//增加hash可以提高安全性
-$password_hash=password_hash($password,PASSWORD_DEFAULT);
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $sql = "SELECT * FROM user WHERE user_name ='".$username."'";
-    $result=mysqli_query($conn,$sql);
-    if(mysqli_num_rows($result)==1 && $password==mysqli_fetch_assoc($result)["password"]){
-        session_start();
-        // Store data in session variables
-        $_SESSION["loggedin"] = true;
-        //這些是之後可以用到的變數
-        $_SESSION["username"] = mysqli_fetch_assoc($result)["username"];
-        header("location:首頁.html");
-    }else{
-            function_alert("帳號或密碼錯誤"); 
-        }
-}
-    else{
-        function_alert("Something wrong"); 
-    }
-
-    // Close connection
-    mysqli_close($link);
-
-function function_alert($message) { 
-      
-    // Display the alert box  
-    echo "<script>alert('$message');
-     window.location.href='index.html';
-    </script>"; 
-    return false;
-} 
+    mysql_close();//關閉資料庫
 ?>
